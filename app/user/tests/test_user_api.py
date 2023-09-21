3d1207ea9d4fe5591e4ef3edf0625a9b0c61a3e6
Nithin T Soman <nithint.soman@dbizsolution.com>
2023-09-21 10:16:44 +0530

    changed code

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -89,7 +89,14 @@ class PublicUserApiTests(TestCase):
         create_user(email='test@example.com', password='goodpass')
 
         payload = {'email': 'test@example.com','password': 'badpass'}
-        res =self.client.post(TOKEN_URL, payload)
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_email_not_found(self):
+        """Test error returned if user not found for given email."""
+        payload = {'email': 'test@example.com', 'password': 'pass123'}
+        res = self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
